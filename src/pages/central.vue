0bc7e57945c412bc724c74175827e6eb7e8d851c
Ryan <Ryan@localhost.com>
2024-04-27 10:19:52 +0700

    Initial commit

---
+++ src/pages/central.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {onMounted} from "vue";
+
+onMounted(() => {
+
+  // Listen to websocket http://localhost:8000/ws
+  const ws = new WebSocket('ws://localhost:8000/ws');
+  ws.onopen = () => {
+    console.log('connected');
+  }
+  ws.onmessage = (event) => {
+    console.log(event.data);
+  }
+  ws.onclose = () => {
+    console.log('disconnected');
+  }
+  ws.onerror = (error) => {
+    console.error(error);
+  }
+
+})
+</script>
+
+<template>
+  <v-container class="fill-height">
+    <v-responsive>
+
+      <h1>Queue</h1>
+
+    </v-responsive>
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
