aabc6dfc077df8ed66d49f924e4801bd096784f4
Ryan <Ryan@localhost.com>
2024-04-28 12:25:01 +0700

    Move the home page to /index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,78 @@
+<script setup lang="ts">
+const exportStats = () =>{
+  console.log("Export PDF")
+  // @app.get('/export/pdf')
+  fetch('http://localhost:8000/export/pdf', {
+    method: 'GET',
+  })
+      .then(response => response.blob())
+      .then(blob => {
+        console.log("Receive file")
+        const url = window.URL.createObjectURL(new Blob([blob]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'stats.pdf');
+        document.body.appendChild(link);
+        link.click();
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+}
+</script>
+
 <template>
-  <HelloWorld />
+  <v-container class="fill-height">
+    <v-responsive>
+
+      <v-img
+          class="mb-4"
+          height="150"
+          src="@/assets/logo.png"
+      />
+      <div class="text-center">
+        <h4 class="text-h4 font-weight-bold">Queue manager</h4>
+        <br/><br/><br/>
+      </div>
+
+
+      <div class="menu">
+        <v-btn
+            to="/central"
+            color="primary"
+        >
+          Central screen
+        </v-btn>
+        <v-btn
+            to="/officer"
+            color="primary"
+        >
+          Officer screen
+        </v-btn>
+        <v-btn
+            to="/distributor"
+            color="primary"
+        >
+          Queue Distributor
+        </v-btn>
+        <v-btn
+            color="primary"
+            @click="exportStats"
+        >
+          <v-icon left>
+            mdi-file-pdf
+          </v-icon>
+          Export Stats as PDF
+        </v-btn>
+      </div>
+    </v-responsive>
+  </v-container>
 </template>
 
-<script lang="ts" setup>
-  //
-</script>
+<style scoped>
+.menu{
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+}
+</style>
